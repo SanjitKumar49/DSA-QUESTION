94b5bf612445844e378a7793a8272fb3a79ee39c
Sanjit Kumar <kumarsanjit18779@gmail.com>
2024-02-05 19:27:31 +0530

    Array

---
+++ C&C++ Program/Voting.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+  int age =2 ;
+  if (age >18)
+  {
+    printf("Yes, You are eligble for voting..");
+  }
+  else
+  {
+    printf("No, You are eligble for voting..");
+  }
+}
